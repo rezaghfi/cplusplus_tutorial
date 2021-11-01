3ce33d186a50a1715f9aeb0d392d477f20e46c1d
reza gholamalitabar <reza.gh.fi@gmail.com>
2021-11-01 21:44:03 +0330

    string example

---
+++ human.hpp
@@ -0,0 +1,23 @@
+#include <string>
+
+class human
+{
+private:
+  /* data */
+  string firstName;
+  string lastName;
+  int age;
+  
+
+public:
+  human(/* args */);
+  ~human();
+};
+
+human::human(/* args */)
+{
+}
+
+human::~human()
+{
+}
---
+++ stringExample.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+namespace reza{
+  string str = "hello reza\n";
+}
+int main() {
+  string greeting = "Hello\n";
+  cout << reza::str;
+  return 0;
+}
