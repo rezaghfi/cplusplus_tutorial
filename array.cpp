f851528cb3c2fe82bc22ff679c9753c4222b6259
reza gholamalitabar <reza.gh.fi@gmail.com>
2022-05-27 07:13:22 +0430

    array in c++

---
+++ array.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+
+int main(){
+  int routerNum = 13;
+  double fc[routerNum];
+  for(int i=0;i<routerNum;i++){
+    fc[i] = 34;
+    std::cout << fc[i] << "\n";
+  }
+  return 0;
+}
