ea5420e63b8b4c0fcb70680ac31762062271a320
reza gholamalitabar <reza.gh.fi@gmail.com>
2023-02-27 11:56:28 +0330

    add jalase1 jalase2

---
+++ jalase2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // first code in c++
+    // variable
+    // identify variable
+    int i = 0;
+    float f = 12.3;
+    double d = 12.3434;
+    char c = 's';
+    string str = "reza";
+    // output
+    cout << f;
+
+    // operator
+    int a = 10;
+    int b = 20;
+    int z = a + b;
+    cout << z;
+
+    // string concat
+    string str1 = "\nhello";
+    string str2 = " reza";
+    string str3 = str1 + str2;
+    cout << str3;
+
+    // data type
+    int number1 = 1;
+    float number2 = 2;
+    double number3 = 23.343434;
+    byte number4;
+    short number5 = 123;
+    long number6 = 123345;
+    bool boolean = true;
+    char character = 'c';
+    string text = "cccc";
+
+    // operators
+    // + - * / ++ -- %
+    number1 = 3;
+    number1 = 3 + 5;
+    number1 = number1 * 2;
+    number1 = a / b;
+    number1++;
+    number1--;
+    number1 = 5;
+    // = -= += *=
+
+
+
+    return 0;
+}
