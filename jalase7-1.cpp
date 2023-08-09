cfad2037261048fb9054db6563a7bb36053b94f2
Reza Gholamalitabar <reza.gh.fi@gmail.com>
2023-08-09 09:30:55 -0400

    jalase7

---
+++ jalase7-1.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal{
+    // field
+// access serectName is private
+private: string secretName;
+    // access of field public
+public: string name;
+
+    // static field
+    bool static live;
+    // method
+    // access of method public
+public: void soundAnimal() {
+        cout << this->name << " sound" << endl;
+    }
+
+    // static method
+    void static staicMethod(){
+        cout << "this is static method" << endl;
+    }
+    // constructor method without input prameter
+    Animal(){
+        cout << "it is constractor without prameter";
+    }
+
+    // constructor with two input parameters
+    Animal(string name, string secret = "secret"){
+        cout << "it is constructor with two parameters" << endl;
+        this->name = name;
+        this->secretName = secret;
+    }
+
+    string getSecretName(){
+        return this->secretName;
+    }
+};
+
+// define fuction
+int sum(int k){
+    int sum = 0;
+    // i++ --> i = i + 1
+    // i +=2 --> i = i + 2
+    for(int i=1; i<=k;i++){
+        sum  = sum + i;
+    }
+    return sum;
+}
+
+int sum2(int k){
+    //recursive
+    if(k==1) return 1;
+    return k + sum2(k-1);
+}
+// function
+int main(){
+    
+    // create instance of Animal
+    Animal dog;
+    dog.soundAnimal();
+    // create instance of Animal
+    Animal cat;
+    cat.name = "persionCat";
+    cat.soundAnimal();
+    dog.name = "jessy";
+    dog.soundAnimal();
+    //dog.secretName = "jan"; it is error for private
+    //Animal::live = true;
+    //cout << Animal::live;
+    Animal::staicMethod();
+    dog.staicMethod();
+    cat.staicMethod();
+    // create instance of Animal
+
+    Animal snake("harry", "harryjon");
+    snake.soundAnimal();
+
+    Animal cow("jack");
+    cow.soundAnimal();
+    // return secrectname private field
+    cout << cow.getSecretName();
+    
+
+    // call function
+    cout << sum(5) << endl;
+    cout << sum2(5) << endl;
+
+
+}
--- jalase7.cpp
+++ jalase7.cpp
@@ -2,90 +2,149 @@
 
 using namespace std;
 
-class Animal{
-    // field
-// access serectName is private
-private: string secretName;
-    // access of field public
-public: string name;
+// global scope
 
-    // static field
-    bool static live;
-    // method
-    // access of method public
-public: void soundAnimal() {
-        cout << this->name << " sound" << endl;
-    }
+int i = 0;
+//  Primitive Built-in Types int , float , double, long, string , bool
 
-    // static method
-    void static staicMethod(){
-        cout << "this is static method" << endl;
-    }
-    // constructor method without input prameter
-    Animal(){
-        cout << "it is constractor without prameter";
-    }
+// Enumerated Types
+enum color {red, green, blue};
+// داخل نوع داده مقادیر ثابت عددی قرار می گیرند.
+enum week {sat =6 , sun, mon, tues, wends , thurs, fri};
 
-    // constructor with two input parameters
-    Animal(string name, string secret = "secret"){
-        cout << "it is constructor with two parameters" << endl;
-        this->name = name;
-        this->secretName = secret;
-    }
+// متغییری که گلوبال تعریف کردیم می توانیم هم نام آن لوکال تعریف کنیم.
+int day[7] = {1,2,3,4,5,6,7};
+//
+string colors[3] = {"red", "green", "blue"};
 
-    string getSecretName(){
-        return this->secretName;
-    }
+// structure
+struct Entity{
+	
+	string name;
+	int legs;
+	
+	void sound(){
+		cout << "------------";
+	}
 };
 
-// define fuction
-int sum(int k){
-    int sum = 0;
-    // i++ --> i = i + 1
-    // i +=2 --> i = i + 2
-    for(int i=1; i<=k;i++){
-        sum  = sum + i;
-    }
-    return sum;
+
+// object oriented programming OOP
+
+class Human{
+
+  public:
+
+  // property
+  int age;
+  string firstname;
+  string lastname;
+
+
+  // methods
+  void walk(){
+    cout << this->firstname << " is walking" << endl;
+  }
+};
+
+class Animal{
+
+public:
+	string color;
+	string name;
+	int legs;
+	
+	// constructor ساختار
+  Animal(){
+    cout << "i'm constructor" << endl;
+  }
+
+	Animal(string name){
+    
+	}
+	
+	Animal(string name, int legs){
+		
+	}
+	void sound(){
+		cout << "animal sound"<< endl;
+	}
+};
+
+// ارث بری در کلاس ها تمام ویژگی ها و متد ها پدر در فرزند استفاده می شوند.
+class Dog: Animal{};
+
+class Cat: Animal{};
+
+class Calculator{
+
+  // نوع دسترسی
+  public:
+
+  Calculator(int in1, int in2){
+    this->input1 = in1;
+    this->input2 = in2;
+  }
+  // property
+  int input1, input2;
+  int output;
+  // methods
+  void sum(){
+    output = input1 + input2;
+  }
+  void mul(){
+    output = input1 * input2;
+  }
+  void sub(){
+    output = input1 - input2;
+  }
+  void div(){
+    output = input1 / input2;
+  }
+};
+
+
+// نحو نوشتار تابع
+void function(int y)
+{
+  cout << " i = " << i << endl;
+  colors[1] = "yellow";
+  cout << colors[1] << endl;
+  color c = blue;
+  cout << "enum data type color = " << c << endl;
+  week day = sun;
+  
+  cout << day << endl;
 }
 
-int sum2(int k){
-    //recursive
-    if(k==1) return 1;
-    return k + sum2(k-1);
-}
-// function
-int main(){
-    
-    // create instance of Animal
-    Animal dog;
-    dog.soundAnimal();
-    // create instance of Animal
-    Animal cat;
-    cat.name = "persionCat";
-    cat.soundAnimal();
-    dog.name = "jessy";
-    dog.soundAnimal();
-    //dog.secretName = "jan"; it is error for private
-    //Animal::live = true;
-    //cout << Animal::live;
-    Animal::staicMethod();
-    dog.staicMethod();
-    cat.staicMethod();
-    // create instance of Animal
+int main()
+{
+  // local scope
+  int i = 1;
+  function(i);
 
-    Animal snake("harry", "harryjon");
-    snake.soundAnimal();
+  // یک نمونه از کلاس ماشین حساب
+  // object or instance
+  Calculator rezasCal(1,60);
+  Calculator tahasCal(2,3);
+  // rezasCal.input1 = 1;
+  // rezasCal.input2 = 50;
+  rezasCal.sum();
+  cout << rezasCal.output << endl;
 
-    Animal cow("jack");
-    cow.soundAnimal();
-    // return secrectname private field
-    cout << cow.getSecretName();
-    
+  // نمونه کلاس انسان
+  Human abolfazl;
+  abolfazl.firstname = "abolfazl";
+  abolfazl.lastname = "rezghi";
+  abolfazl.age = 16;
+  abolfazl.walk();
 
-    // call function
-    cout << sum(5) << endl;
-    cout << sum2(5) << endl;
+  // animal
+  Animal unknown;
 
-
+  Cat catty;
+  catty.name = "catty";
+  catty.color = "gray";
+  catty.sound();
+  return 0;
 }
